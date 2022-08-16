eac90ded5ba6061435a99b8b4440984b9223a7ec
laiz-in <laizin2107@gmail.com>
2022-08-16 17:51:03 +0400

    code_push_test

---
+++ mongotestfile.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://laizinv:laizin2107@cluster0.j7mdzpr.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+d ={
+    "name" : "laizin",
+    "number" : "8113937942",
+    "mail_id" : "laizin2107@gmail.com"
+   }
+database1 = client['test_database']
+collections_1 = database1['test_database']
+collections_1.insert_one(d)
